f05d9d7f283eefe26d818bd0d2d466d84cb77090
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-10-24 00:23:21 -0500

    Added spec support require, removed dummy spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
 require 'black_box'
+
+%w(../support/**/*.rb).each do |path|
+  Dir[File.expand_path(path, __FILE__)].each { |f| require f }
+end
