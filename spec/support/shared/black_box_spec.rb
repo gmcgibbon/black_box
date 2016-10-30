9781a0fb49f66f749812a81e83682ae04dbde0f5
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-10-30 14:19:42 -0500

    Added .expose tests to black_box shared spec

--- spec/support/shared/black_box_spec.rb
+++ spec/support/shared/black_box_spec.rb
@@ -64,4 +64,44 @@ shared_examples 'black_box' do
     end
   end
 
+  describe '.expose' do
+
+    let(:methods) { %i(size any? empty?) }
+
+    before do
+      subject.subject klass
+      subject.box_attributes.clear
+      subject.expose *methods
+    end
+
+    it 'should push methods to box_methods' do
+      expect(subject.box_methods).to eq methods
+    end
+
+    it 'should delegate methods' do
+      methods.each do |method|
+        expect(subject).to delegate_method(method).to :instance
+      end
+    end
+
+    context 'duplicate keys' do
+
+      let(:methods) { %i(size size any? empty?) }
+
+      it 'should push unique methods to box_methods' do
+        expect(subject.box_methods).to_not eq methods
+        expect(subject.box_methods).to eq methods.uniq
+      end
+    end
+
+    context 'instance' do
+
+      it 'should delegate methods' do
+        methods.each do |method|
+          expect(instance).to delegate_method(method).to :subject
+        end
+      end
+    end
+  end
+
 end
