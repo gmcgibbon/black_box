ccb1618cb6208103ce423aa54e6a0a5fa03bab85
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-10-31 01:28:18 -0500

    Added mocked forwarding specs for segment #identify and #track

--- spec/integration/segment/analytics_spec.rb
+++ spec/integration/segment/analytics_spec.rb
@@ -13,4 +13,38 @@ describe Analytics do
     attributes: %i(write_key),
     methods:    %i(identify track)
   )
+
+  describe '#identify' do
+
+    let(:params) do
+      { user_id: 0, attributes: { name: 'mock' } }
+    end
+
+    before do
+      expect(
+        described_class.instance.subject
+      ).to receive(:identify).with(params)
+    end
+
+    it 'should forward to subject' do
+      expect { described_class.identify params }.to_not raise_error
+    end
+  end
+
+  describe '#track' do
+
+    let(:params) do
+      { user_id: 0, event: 'mock', properties: { type: 'mock' } }
+    end
+
+    before do
+      expect(
+        described_class.instance.subject
+      ).to receive(:track).with(params)
+    end
+
+    it 'should forward to subject' do
+      expect { described_class.track params }.to_not raise_error
+    end
+  end
 end
