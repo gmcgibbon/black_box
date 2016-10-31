141d37a0892aeae0afe4b38538295bb1c6adb4e4
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2016-10-31 01:23:12 -0500

    Added mocked forwarding specs for pubnub #publish and #subscribe

--- spec/integration/pubnub/realtime_spec.rb
+++ spec/integration/pubnub/realtime_spec.rb
@@ -17,4 +17,42 @@ describe Realtime do
     attributes: %i(subscribe_key publish_key secret_key logger ssl),
     methods:    %i(publish subscribe)
   )
+
+  describe '#publish' do
+
+    let(:params) do
+      { channel: 'mock', message: 'hello world!' }
+    end
+
+    before do
+      expect(
+        described_class.instance.subject
+      ).to receive(:publish).with(params)
+    end
+
+    it 'should forward to subject' do
+      expect { described_class.publish params }.to_not raise_error
+    end
+  end
+
+  describe '#subscribe' do
+
+    let(:params) do
+      { channel: 'mock' }
+    end
+
+    let(:block) do
+      -> (message) { }
+    end
+
+    before do
+      expect(
+        described_class.instance.subject
+      ).to receive(:subscribe).with(params, &block)
+    end
+
+    it 'should forward to subject' do
+      expect { described_class.subscribe params, &block }.to_not raise_error
+    end
+  end
 end
